0d1ee10550e779e6b596207252c380a5e7bb2556
Shivam Mishra <scm.mymail@gmail.com>
2023-06-06 21:44:04 +0530

    feat: setup histoire

---
+++ src/components/Button/Button.story.vue
@@ -0,0 +1,9 @@
+<template>
+  <Story>
+    <Button text="hello" />
+  </Story>
+</template>
+
+<script lang="ts" setup>
+import Button from "./Button.vue";
+</script>
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <button class="">
+    <slot>{{ text }}</slot>
+  </button>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  size: "large" | "medium" | "small" | "x-small";
+  style: "primary" | "secondary" | "clear" | "warning" | "danger" | "success";
+  disabled: boolean;
+  text: string;
+}>();
+</script>
