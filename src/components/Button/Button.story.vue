85a7ca63a81c6aaab9ed7be943e88a3f269d4257
Shivam Mishra <scm.mymail@gmail.com>
2023-06-07 22:10:28 +0530

    feat: update button

--- src/components/Button/Button.story.vue
+++ src/components/Button/Button.story.vue
@@ -1,9 +1,71 @@
+<script lang="ts" setup>
+import { reactive } from "vue";
+import Button from "./Button.vue";
+
+const state = reactive({
+  disabled: false,
+  text: "Joy Button",
+  size: "medium",
+  fullWidth: false,
+  loading: false,
+  icon: "heroicons:check-circle",
+  trailingIcon: false,
+});
+
+const styleOptions = [
+  "primary",
+  "secondary",
+  "clear",
+  "warning",
+  "danger",
+  "success",
+];
+
+function updateClicked() {
+  alert("Clicked");
+}
+</script>
+
 <template>
-  <Story>
-    <Button text="hello" />
+  <Story title="Base Components/Button" :layout="{ type: 'grid', width: 400 }">
+    <template #controls>
+      <HstSelect
+        v-model="state.size"
+        :title="'Size'"
+        :options="{
+          large: 'Large',
+          medium: 'Medium',
+          small: 'Small',
+        }"
+      />
+      <HstText v-model="state.text" title="Text" />
+      <HstCheckbox v-model="state.disabled" title="Disabled" />
+      <HstCheckbox v-model="state.fullWidth" title="Full Width" />
+      <HstCheckbox v-model="state.loading" title="Loading" />
+      <div>
+        <h5 class="px-2 mt-5 text-xs tracking-widest text-gray-300 uppercase">
+          Icon Options
+        </h5>
+        <HstText v-model="state.icon" title="Icon" />
+        <HstCheckbox v-model="state.trailingIcon" title="Trailing Icon" />
+      </div>
+    </template>
+
+    <Variant v-for="style in styleOptions" :title="style" autoPropsDisabled>
+      <div class="flex items-center justify-center p-5">
+        <Button
+          :size="state.size"
+          :style="style"
+          :disabled="state.disabled"
+          :fullWidth="state.fullWidth"
+          :loading="state.loading"
+          :icon="state.icon"
+          :trailingIcon="state.trailingIcon"
+          @click="updateClicked"
+        >
+          {{ state.text }}
+        </Button>
+      </div>
+    </Variant>
   </Story>
 </template>
-
-<script lang="ts" setup>
-import Button from "./Button.vue";
-</script>
--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -1,14 +1,87 @@
+<script lang="ts" setup>
+import { Icon } from "@iconify/vue";
+import { computed } from "vue";
+
+type ButtonSizes = "large" | "medium" | "small";
+type ButtonStyles =
+  | "primary"
+  | "secondary"
+  | "clear"
+  | "warning"
+  | "danger"
+  | "success";
+
+interface ButtonProps {
+  size?: ButtonSizes;
+  style?: ButtonStyles;
+  disabled?: boolean;
+  text?: string;
+  fullWidth?: boolean;
+  loading?: boolean;
+  icon?: string;
+  trailingIcon?: boolean;
+}
+
+const props = withDefaults(defineProps<ButtonProps>(), {
+  size: "medium",
+  style: "primary",
+  disabled: false,
+  text: "Button",
+  fullWidth: false,
+  loading: false,
+  trailingIcon: false,
+  icon: "",
+});
+
+const isLoading = computed(() => props.loading && !props.disabled);
+</script>
+
 <template>
-  <button class="">
-    <slot>{{ text }}</slot>
+  <button
+    class="inline-flex items-center justify-center gap-1.5 transition-colors duration-200 rounded-lg with-icon focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+    :class="{
+      'px-4 py-3 text-sm leading-6 h-12': size === 'large',
+      'px-4 py-2 text-sm leading-6 h-10': size === 'medium',
+      'px-3 py-1 text-sm leading-6 h-8': size === 'small',
+      'bg-blue-500 hover:bg-blue-600 text-white':
+        style === 'primary' && !disabled,
+      'bg-transparent hover:bg-gray-50 text-gray-800':
+        style === 'clear' && !disabled,
+      'bg-white hover:bg-gray-50 text-gray-800 border border-gray-200 shadow-clear-button':
+        style === 'secondary' && !disabled,
+      'bg-orange-500 hover:bg-orange-600 text-white':
+        style === 'warning' && !disabled,
+      'bg-red-500 hover:bg-red-600 text-white': style === 'danger' && !disabled,
+      'bg-green-500 hover:bg-green-600 text-white':
+        style === 'success' && !disabled,
+      'bg-gray-100 text-gray-300 cursor-not-allowed': disabled,
+      'w-full flex justify-center': fullWidth,
+      'flex-row-reverse': trailingIcon && icon,
+      relative: isLoading,
+    }"
+    :aria-label="text"
+  >
+    <Icon
+      v-if="isLoading"
+      icon="lucide:loader-2"
+      class="absolute w-4 h-4 animate-spin"
+    />
+    <Icon
+      v-if="icon"
+      :icon="icon"
+      class="w-4 h-4"
+      :class="{
+        invisible: isLoading,
+      }"
+    />
+    <span
+      :class="{
+        invisible: isLoading,
+      }"
+    >
+      <slot>
+        {{ text }}
+      </slot>
+    </span>
   </button>
 </template>
-
-<script lang="ts" setup>
-defineProps<{
-  size: "large" | "medium" | "small" | "x-small";
-  style: "primary" | "secondary" | "clear" | "warning" | "danger" | "success";
-  disabled: boolean;
-  text: string;
-}>();
-</script>
