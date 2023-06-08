f6fd8686376de65f0331923d539f92f73f99dd37
Shivam Mishra <scm.mymail@gmail.com>
2023-06-08 20:05:02 +0530

    feat: add avatar

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import UButton from "./components/Base/Button/Button.vue";
 import UIcon from "./components/Base/Icon/Icon.vue";
+import UAvatar from "./components/Base/Avatar/Avatar.vue";
+
 import "./tailwind.css";
 
-export { UButton, UIcon };
+export { UButton, UIcon, UAvatar };
