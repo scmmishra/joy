b466ec66fb0d0be770f799a2f8e12d7eacdd9f9e
Shivam Mishra <scm.mymail@gmail.com>
2023-06-09 20:34:18 +0530

    feat: configure histoire theme

--- histoire.config.ts
+++ histoire.config.ts
@@ -1,7 +1,16 @@
+import { defaultColors } from "histoire";
+
 import { defineConfig } from "histoire";
 import { HstVue } from "@histoire/plugin-vue";
 
 export default defineConfig({
   plugins: [HstVue()],
   setupFile: "/src/setup.ts",
+  theme: {
+    title: "Joy Design System.",
+    defaultColorScheme: "light",
+    colors: {
+      primary: defaultColors.blue,
+    },
+  },
 });
