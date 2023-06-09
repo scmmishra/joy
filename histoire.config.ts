c9be2237f437d8a17bee1823c36fc592becac97b
Shivam Mishra <scm.mymail@gmail.com>
2023-06-09 20:43:23 +0530

    feat: remove color switcher

--- histoire.config.ts
+++ histoire.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   theme: {
     title: "Joy Design System.",
     defaultColorScheme: "light",
+    hideColorSchemeSwitch: true,
     favicon: "./public/joy.png",
     colors: {
       primary: defaultColors.blue,
