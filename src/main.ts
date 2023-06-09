6d6f715ed1922ef48e7cc59e7b6d603fa3300dbe
Shivam Mishra <scm.mymail@gmail.com>
2023-06-09 20:18:55 +0530

    feat: export avatar group

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import UButton from "./components/Base/Button/Button.vue";
 import UIcon from "./components/Base/Icon/Icon.vue";
 import UAvatar from "./components/Base/Avatar/Avatar.vue";
+import UAvatarGroup from "./components/Base/Avatar/AvatarGroup.vue";
 
 import "./tailwind.css";
 
-export { UButton, UIcon, UAvatar };
+export { UButton, UIcon, UAvatar, UAvatarGroup };
