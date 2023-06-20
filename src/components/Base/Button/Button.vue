4a67b52b2b0b7f8ee41af164a395a71b5d18b77b
Shivam Mishra <scm.mymail@gmail.com>
2023-06-20 20:33:16 +0530

    feat: update button colors

--- src/components/Base/Button/Button.vue
+++ src/components/Base/Button/Button.vue
@@ -12,7 +12,7 @@ const props = withDefaults(defineProps<ButtonProps>(), {
   fullWidth: false,
   loading: false,
   trailingIcon: false,
-  icon: "",
+  icon: undefined,
   elevated: false,
 });
 
@@ -22,21 +22,21 @@ const classes: Record<ButtonColors, Record<ButtonVariants, string>> = {
   primary: {
     solid: "bg-blue-500 hover:bg-blue-600 active:bg-blue-600 text-white",
     outline:
-      "hover:bg-blue-50 active:bg-blue-50 text-blue-500 border border-blue-500",
+      "hover:bg-blue-50 active:bg-blue-50 text-blue-500 border border-blue-300",
     faded: "bg-blue-50 text-blue-500 hover:bg-blue-100 active:bg-blue-100",
     clear: "bg-transparent hover:bg-blue-50 active:bg-blue-50 text-blue-500",
   },
   secondary: {
     solid: "bg-gray-50 hover:bg-gray-100 active:bg-gray-100 text-gray-600",
     outline:
-      "hover:bg-gray-100 active:bg-gray-50 text-gray-800 border border-gray-500",
+      "hover:bg-gray-100 active:bg-gray-50 text-gray-800 border border-gray-200",
     faded: "bg-gray-50 text-gray-500 hover:bg-gray-100 active:bg-gray-100",
     clear: "bg-transparent hover:bg-gray-50 active:bg-gray-50 text-gray-500",
   },
   warning: {
     solid: "bg-orange-500 hover:bg-orange-600 active:bg-orange-600 text-white",
     outline:
-      "hover:bg-orange-50 active:bg-orange-50 text-orange-600 border border-orange-500",
+      "hover:bg-orange-50 active:bg-orange-50 text-orange-600 border border-orange-300",
     faded:
       "bg-orange-50 text-orange-500 hover:bg-orange-100 active:bg-orange-100",
     clear:
@@ -45,14 +45,14 @@ const classes: Record<ButtonColors, Record<ButtonVariants, string>> = {
   danger: {
     solid: "bg-red-500 hover:bg-red-600 active:bg-red-600 text-white",
     outline:
-      "hover:bg-red-50 active:bg-red-50 text-red-600 border border-red-500",
+      "hover:bg-red-50 active:bg-red-50 text-red-600 border border-red-300",
     faded: "bg-red-50 text-red-500 hover:bg-red-100 active:bg-red-100",
     clear: "bg-transparent hover:bg-red-50 active:bg-red-50 text-red-500",
   },
   success: {
     solid: "bg-green-500 hover:bg-green-600 active:bg-green-600 text-white",
     outline:
-      "hover:bg-green-50 active:bg-green-50 text-green-600 border border-green-500",
+      "hover:bg-green-50 active:bg-green-50 text-green-600 border border-green-300",
     faded: "bg-green-50 text-green-500 hover:bg-green-100 active:bg-green-100",
     clear: "bg-transparent hover:bg-green-50 active:bg-green-50 text-green-500",
   },
