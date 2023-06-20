1334772eb200b6d7283dcac2b77ca0862fcb4bd7
Shivam Mishra <scm.mymail@gmail.com>
2023-06-20 23:31:44 +0530

    feat: export badge

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import UButton from "./components/Base/Button/Button.vue";
 import UIcon from "./components/Base/Icon/Icon.vue";
 import UAvatar from "./components/Base/Avatar/Avatar.vue";
 import UAvatarGroup from "./components/Base/Avatar/AvatarGroup.vue";
+import UBadge from "./components/Base/Badge/Badge.vue";
+import UBadgeContainer from "./components/Base/Badge/BadgeContainer.vue";
 
 import "./tailwind.css";
 
-export { UButton, UIcon, UAvatar, UAvatarGroup };
+export { UButton, UIcon, UAvatar, UAvatarGroup, UBadge, UBadgeContainer };
